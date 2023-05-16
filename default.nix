7932f4184ed174a37039b1700592cb698c77a53b
Rasmus Lövegren <rasmus@lovegren.fi>
2023-05-16 16:12:38 +0300

    Nix environment

---
+++ default.nix
@@ -0,0 +1,23 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz );
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustStableChannel = (nixpkgs.rustChannels.stable).rust.override {
+    extensions = [
+      "rust-src"
+      "rust-analysis"
+      "rustfmt-preview"
+      "clippy-preview"
+    ];
+    targets = [
+      "x86_64-unknown-linux-gnu"
+      "x86_64-unknown-linux-musl"
+    ];
+  };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "env";
+    buildInputs = [
+      rustStableChannel
+    ];
+  }
